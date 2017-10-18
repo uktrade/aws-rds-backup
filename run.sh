ca9def3a9474f6d6fb39bfadb303ce29d090f65f
Bowen Sun <bowensun@gmail.com>
2017-10-18 02:12:44 +0100

    * filter new snapshot doesn't have SnapshotCreateTime key

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ export EXPIRE_DATE=$(date -d "-$RDS_RETENTION days" +%s)
 
 aws rds create-db-snapshot --db-instance-identifier $RDS_ID --db-snapshot-identifier "$RDS_ID-$DATE"
 
-EXIPRED=$(aws rds describe-db-snapshots | jq -rC '.DBSnapshots[] | select(.DBInstanceIdentifier==env.RDS_ID) | select((.SnapshotCreateTime|sub("(\\.[0-9]{3})"; ""; "x")|strptime("%Y-%m-%dT%H:%M:%SZ")|mktime) < (env.EXPIRE_DATE|tonumber)) | .DBSnapshotIdentifier')
+EXIPRED=$(aws rds describe-db-snapshots | jq -rC '.DBSnapshots[] | select(.DBInstanceIdentifier==env.RDS_ID) | select(has("SnapshotCreateTime")==true) | select((.SnapshotCreateTime|sub("(\\.[0-9]{3})"; ""; "x")|strptime("%Y-%m-%dT%H:%M:%SZ")|mktime) < (env.EXPIRE_DATE|tonumber)) | .DBSnapshotIdentifier')
 
 for X in $EXIPRED; do
   aws rds delete-db-snapshot --db-snapshot-identifier "$X"
